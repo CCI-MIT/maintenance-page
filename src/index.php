8dfb2c3ccdb8c1c90918565476c33a17fafd1c16
Johannes Bachhuber <jobachhu@mit.edu>
2018-04-18 12:30:13 +0200

    Simplify cache headers

--- src/index.php
+++ src/index.php
@@ -1,8 +1,6 @@
 <?php
 http_response_code(503);
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
+header("Cache-Control: no-store, max-age=0");
 
 if (file_exists("../config.ini")):
     $config = parse_ini_file("../config.ini");
